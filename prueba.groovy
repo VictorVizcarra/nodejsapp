6605caa7e775dca7dc5b04b73ac975b3cbf3c192
Victor Vizcarra <54164831+victorvizcarra@users.noreply.github.com>
2022-04-28 17:52:16 -0700

    Update prueba.groovy

--- prueba.groovy
+++ prueba.groovy
@@ -1,36 +1,37 @@
-pipeline { 
-    environment { 
-        registry = "victor09vizcarra/nodejsapp"
-        registryCredential = 'docker-hub' 
-        dockerImage = '' 
-    }
-    agent any 
-    stages { 
-        stage('Cloning our Git') { 
-            steps { 
-                git 'https://github.com/victorvizcarra/nodejsapp.git' 
-            }
-        } 
-        stage('Building our image') { 
-            steps { 
-                script { 
-                    dockerImage = docker.build registry + ":$BUILD_NUMBER" 
-                }
-            } 
-        }
-        stage('Deploy our image') { 
-            steps { 
-                script { 
-                    docker.withRegistry( '', registryCredential ) { 
-                        dockerImage.push() 
-                    }
-                } 
-            }
-        } 
-        stage('Cleaning up') { 
-            steps { 
-                sh "docker rmi $registry:$BUILD_NUMBER" 
-            }
-        } 
-    }
+pipeline {
+environment {
+imagename = "victor09vizcarra/nodejsapp"
+registryCredential = 'docker-hub'
+dockerImage = ''
+}
+agent any
+stages {
+stage('Cloning Git') {
+steps {
+git([url: 'https://github.com/victorvizcarra/nodejsapp.git', branch: 'master', credentialsId: 'GitHub'])
+}
+}
+stage('Building image') {
+steps{
+script {
+dockerImage = docker.build imagename
+}
+}
+}
+stage('Deploy Image') {
+steps{
+script {
+docker.withRegistry( '', registryCredential ) {
+dockerImage.push("$BUILD_NUMBER")
+dockerImage.push('latest')
+}
+}
+}
+}
+stage('Remove Unused docker image') {
+steps{
+sh "docker rmi $imagename:$BUILD_NUMBER"
+sh "docker rmi $imagename:latest"
+}
+}
 }
